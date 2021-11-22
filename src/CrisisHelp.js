76b0a68117abe04de631bca1cc158acb9bb7e7c6
40203858 <emoran05@qub.ac.uk>
2021-11-22 20:42:01 +0000

    Included some hard coded examples of crisis services for demo purpose.

--- src/CrisisHelp.js
+++ src/CrisisHelp.js
@@ -7,7 +7,34 @@ export default function crisisHelp() {
       );
    return( 
 
-    <h2>crisis Details to be added</h2>
+    <div>
+  <h2>Crisis Metal Health Services</h2>
+
+  <table>
+    <tr>
+      <th>Service</th>
+      <th>&nbsp;&nbsp;Contact Number</th>
+    </tr>
+    <tbody>
+      <tr>
+        <td>Action Mental Health</td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;90745015</td>
+      </tr>
+      <tr>
+        <td>LAMP - Life After Mental Health Problems</td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;90242982</td>
+      </tr>
+      <tr>
+        <td>LMen’s Advisory Project </td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;90241929</td>
+      </tr>
+      <tr>
+        <td>NI Association Mental Health</td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;90328474</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 
    );
 }
