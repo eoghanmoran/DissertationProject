185419232243698e55c5d08a6c05fe536c7b9ff4
40203858 <emoran05@qub.ac.uk>
2022-01-13 16:53:05 +0000

    Firestore config added

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAz1m-CnM42RLjBxUYRGu3j3tjDejGcPlE",
+    authDomain: "forward-south.firebaseapp.com",
+    databaseURL: "https://forward-south-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "forward-south",
+    storageBucket: "forward-south.appspot.com",
+    messagingSenderId: "148774866256",
+    appId: "1:148774866256:web:d08452cc4d18a9097eb055",
+    measurementId: "G-BK799ZG0X9"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
