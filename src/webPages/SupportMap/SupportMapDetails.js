2a029042ab2d6e5402fc0cca6e5bde196a990d46
40203858 <emoran05@qub.ac.uk>
2022-01-17 14:28:44 +0000

    Google map code added to support map page

--- src/webPages/SupportMap/SupportMapDetails.js
+++ src/webPages/SupportMap/SupportMapDetails.js
@@ -1,28 +1,88 @@
 
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
 import '../../App.css';
+import * as parkData from "../../data/hospitals.json";
+import mapStyles from "./mapStyles";
 
 
-const mapStyles = {
-  width: '30%',
-  height: '30%',
-  justifyContent: "center",
-  alignItems: "center",
-  padding: "0.5rem",
-  margin: "1rem",
-};
 
-export class SupportMapDetails extends Component {
+function Map() {
+  const [selectedPark, setSelectedPark] = useState(null);
 
-  render() {
-    return (
-      <div className="mapwrapper">
-      </div>
-    );
-  }
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectedPark(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
+  return (
+
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 54.585030, lng: -5.935540 }}
+
+    >
+      {parkData.features.map(park => (
+        <Marker
+          key={park.properties.PARK_ID}
+          position={{
+            lat: park.geometry.coordinates[1],
+            lng: park.geometry.coordinates[0]
+          }}
+          onClick={() => {
+            setSelectedPark(park);
+          }}
+        />
+      ))}
+
+      {selectedPark && (
+        <InfoWindow
+          onCloseClick={() => {
+            setSelectedPark(null);
+          }}
+          position={{
+            lat: selectedPark.geometry.coordinates[1],
+            lng: selectedPark.geometry.coordinates[0]
+          }}
+        >
+          <div>
+            <h2>{selectedPark.properties.NAME}</h2>
+            <p>{selectedPark.properties.DESCRIPTIO}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
 }
 
-export default GoogleApiWrapper({
-  apiKey: ''
-})(SupportMapDetails);
+const MapWrapped = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  return (
+
+    <div style={{ width: "50vw", height: "75vh" }}>
+      <h1>Hospital</h1>
+      <MapWrapped
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY
+          }`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
