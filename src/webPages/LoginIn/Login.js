44f616014eda313e293f993e134780964aaeff00
40203858 <emoran05@qub.ac.uk>
2022-01-19 15:57:10 +0000

    code to log user in added

--- src/webPages/LoginIn/Login.js
+++ src/webPages/LoginIn/Login.js
@@ -6,48 +6,89 @@ import Checkbox from '@material-ui/core/Checkbox';
 import Footer from '../../components/Footer'
 import logo from '../../../src/images/logosmall.png';
 import '../../App.css';
+import { useState } from "react";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut,
+} from "firebase/auth";
+import { auth } from "./firebase-config";
 
-export default class Login extends Component {
-    render() {
-        const paperStyle = { padding: 20, height: '70vh', width: 280, margin: "20px auto" }
-        const avatarStyle = { backgroundColor: '#1bbd7e' }
-        const btnstyle = { margin: '8px 0' }
-        return (
-            <div className="background">
-                <div className="container">
-                    <Grid>
-                        <Paper elevation={10} style={paperStyle}>
-                            <Grid align="center">
-                                <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
-                                <h2>Sign In</h2>
-                            </Grid>
-                            <TextField label='Username' placeholder='Enter username' fullWidth required />
-                            <TextField label='Password' placeholder='Enter password' type='password' fullWidth required />
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        name="checkedB"
-                                        color="primary"
-                                    />
-                                }
-                                label="Remember me"
-                            />
-                            <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
-                            <Typography >
-                                <Link href="#" >
-                                    Forgot password ?
-                                </Link>
-                            </Typography>
-                            <Typography > Do you have an account ?
-                                <Link href="#" >
-                                    Sign Up
-                                </Link>
-                            </Typography>
-                        </Paper>
-                    </Grid>
-                    <Footer />
-                </div>
+function App() {
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+    const [user, setUser] = useState({});
+
+    onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+    });
+
+    const login = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(
+                auth,
+                loginEmail,
+                loginPassword
+            );
+            console.log(user);
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    const logout = async () => {
+        await signOut(auth);
+    };
+
+    const paperStyle = { padding: 20, height: '60vh', width: 280, margin: "20px auto" }
+    const avatarStyle = { backgroundColor: '#1bbd7e' }
+    const btnstyle = { margin: '8px 0' }
+    return (
+        <div className="background">
+            <div className="container">
+                <Grid>
+                    <Paper elevation={10} style={paperStyle}>
+                        <Grid align="center">
+                            <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
+                            <h2>Sign In</h2>
+                        </Grid>
+
+                        <TextField label='Email' placeholder='Enter Email' fullWidth required
+                            onChange={(event) => {
+                                setLoginEmail(event.target.value);
+                            }}
+                        />
+                        <TextField label='Password' placeholder='Enter password' type='password' fullWidth required
+                            onChange={(event) => {
+                                setLoginPassword(event.target.value);
+                            }}
+                        />
+
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    name="checkedB"
+                                    color="primary"
+                                />
+                            }
+                            label="Remember me"
+                        />
+                        <Button onClick={login} type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+                        <Button onClick={logout} type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign out</Button>
+
+                        <Grid align="center">
+                            <h6> User Logged In: </h6>
+                            <h6>{user?.email}</h6>
+                        </Grid>
+                    </Paper>
+                </Grid>
+                <Footer />
             </div>
-        )
-    }
+        </div>
+    )
+
 }
+export default App;
