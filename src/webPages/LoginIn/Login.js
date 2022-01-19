f34ad829ca3ee254c7412c7d5e668c0572bbaec7
40203858 <emoran05@qub.ac.uk>
2022-01-19 15:16:59 +0000

    Footer added into Admin Login page

--- src/webPages/LoginIn/Login.js
+++ src/webPages/LoginIn/Login.js
@@ -45,6 +45,7 @@ export default class Login extends Component {
                             </Typography>
                         </Paper>
                     </Grid>
+                    <Footer />
                 </div>
 
             </div>
