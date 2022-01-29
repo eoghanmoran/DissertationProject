2bb6692fdd854e1b5c0103681ea58bd67fa64fec
40203858 <emoran05@qub.ac.uk>
2022-01-29 23:55:42 +0000

    Contact page upgrades

--- src/webPages/ContactForm/ContactFormPage.js
+++ src/webPages/ContactForm/ContactFormPage.js
@@ -1,92 +1,45 @@
 import React from "react";
-import ContactForm from "./ContactFormDetails";
-import { ThemeProvider, makeStyles } from '@material-ui/styles';
-import { createTheme } from "@mui/material/styles";
-import { Typography } from '@material-ui/core';
-import Footer from '../../components/Footer'
-import { init } from 'emailjs-com';
-import logo from '../../../src/images/logosmall.png';
-import '../../App.css';
+import Map from "../SupportMap/SupportMapDetails";
+//import PizzaLeft from "../../images/background.png";
+import "./Contact.css";
 
-//  init(""); 
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#2e1667",
-    },
-    secondary: {
-      main: "#c7d8ed",
-    },
-  },
-  typography: {
-    fontFamily: [
-      'Roboto'
-    ],
-    h4: {
-      fontSize: '20px',
-      fontWeight: 600,
-      lineHeight: '2rem',
-    },
-    h5: {
-      fontWeight: 300,
-      lineHeight: '2rem',
-    },
-  },
-});
-
-const styles = makeStyles({
-  wrapper: {
-    width: "100%",
-    margin: "auto",
-    textAlign: "center"
-  },
-  bigSpace: {
-
-  },
-  littleSpace: {
-    marginTop: "2.5rem",
-  },
-  grid: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexWrap: "wrap",
-  },
-})
-
-
-export default function contactForm() {
-  const classes = styles();
-
-
+function Contact() {
   return (
+    <div className="contact">
+      <div
+        className="leftSide"
+      //style={{ backgroundImage: `url(${PizzaLeft})` }}
+      >
+        <iframe
+          class="embed-responsive-item"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2312.0173093746307!2d-5.936617048947077!3d54.58606908873286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x486108f03ea92097%3A0x420253dcb69b79af!2sForward%20South%20Partnership!5e0!3m2!1sen!2suk!4v1643499654102!5m2!1sen!2suk"
+          width="100%"
+          height="100%"
+          frameborder="0"
+          // aria-hidden="false"
+          // tabindex="0"
+          allowfullscreen=""
+        ></iframe>
+      </div>
+      <div className="rightSide">
+        <h1> Contact Us</h1>
 
-    <div className="background">
-      <div className="container">
-
-        <div className="App">
-
-          <ThemeProvider theme={theme}>
-
-            <div className={classes.wrapper}>
-
-              <div className="logo">
-                <img src={logo} alt="this is a logo image" />
-              </div>
-
-              <Typography variant="h4" className={classes.bigSpace} color="primary">
-                Forward south Partnership
-              </Typography>
-
-              <ContactForm />
-            </div>
-          </ThemeProvider>
-        </div>
-        <Footer />
+        <form id="contact-form" method="POST">
+          <label htmlFor="name">Full Name</label>
+          <input name="name" type="text" />
+          <label htmlFor="email">Email</label>
+          <input name="email"  type="email" />
+          <label htmlFor="message">Message</label>
+          <textarea
+            rows="6"
+            name="message"
+            required
+          ></textarea>
+          <button type="submit"> Send Message</button>
+        </form>
       </div>
     </div>
-
   );
 }
 
+export default Contact;
