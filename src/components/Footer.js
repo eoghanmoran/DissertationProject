b25133b3c721278f9416e02017ed029567fdce87
mildredtuppal <40262575@ads.qub.ac.uk>
2022-02-12 03:10:11 +0000

    Successfully fixed the Addiction.js file Note: (To be continued)..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,23 @@ import '../App.css';
 
 
 import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import FacebookIcon from '@mui/icons-material/Facebook';
 
 function Footer() {
     return (
-  
+        
         <BottomNavigation >
-         <a href="https://twitter.com/SouthBelfastPB?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><BottomNavigationAction label="Twitter" value="favorites" icon={<TwitterIcon  style={{fill: "#1DA1F2"}}/>}/> </a> 
-         <a href= "https://www.facebook.com/SouthBelfastPartnership"> <BottomNavigationAction label="Facebook" value="nearby" icon={<FacebookIcon  style={{fill: " #1DA1F2"}}/>}/> </a>
-         <a href="https://www.youtube.com/channel/UCU7YmFwD-0HO4KKa4GWvHPA"><BottomNavigationAction label="YouTube" value="folder" icon={<YouTubeIcon  style={{fill: "#c4302b"}}/>} /> </a> 
+          <a href="https://twitter.com/SouthBelfastPB?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><BottomNavigationAction label="Twitter" value="favorites" icon={<TwitterIcon  style={{fill: "#1DA1F2"}}/>}/> </a> 
+         <a href = "https://www.facebook.com/SouthBelfastPartnership/"><BottomNavigationAction label="Facebook" value="nearby" icon={<FacebookIcon  style={{fill: " #1DA1F2"}}/>} /> </a>
+         <a href="https://www.youtube.com/channel/UCU7YmFwD-0HO4KKa4GWvHPA"><BottomNavigationAction label="YouTube" value="folder" icon={<YouTubeIcon  style={{fill: "#c4302b"}}/>}/> </a> 
+
+
         </BottomNavigation>
+      
+  
+
       
 )
 }
