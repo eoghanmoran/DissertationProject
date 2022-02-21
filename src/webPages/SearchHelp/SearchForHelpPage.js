3b163223f966915edf6ba6652073f1d67fe0c900
40203858 <emoran05@qub.ac.uk>
2022-02-21 22:23:14 +0000

    Updates to search

--- src/webPages/SearchHelp/SearchForHelpPage.js
+++ src/webPages/SearchHelp/SearchForHelpPage.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Search from "./SearchBar"
-
+import Footer from '../../components/Footer'
 function App() {
   return (
-    <>
-      <Search />
-    </>
+    
+      <div className="background">
+        <div className="container">
+          <div className="whiteBackground">
+          <Search />
+          </div>
+          <Footer/>
+        </div>
+
+      </div>
+      
+    
   );
 }
 
