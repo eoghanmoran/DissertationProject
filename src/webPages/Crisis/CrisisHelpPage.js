8f5c87b2080c832dc74ab900ec43fe5adff4e82b
40203858 <emoran05@qub.ac.uk>
2022-03-08 23:29:13 +0000

    Updated edit benefits page to enable editing of description in this table

--- src/webPages/Crisis/CrisisHelpPage.js
+++ src/webPages/Crisis/CrisisHelpPage.js
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import { db } from "../../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 import './Crisis.css';
-import Table from 'react-bootstrap/Table'
-
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import Container from 'react-bootstrap/Container'
 
 
 function CrisishelpDetails() {
@@ -21,30 +22,31 @@ function CrisishelpDetails() {
   }, []);
 
   return (
-    <div className="background">
-      <div className="container">
-        <Table striped bordered responsive="sm">
-          <thead>
-            <tr>
-              <th>Service</th>
-              <th>Contact Number</th>
-              <th>Description</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Helplines.map((helpline) => {
-              return (
-                <tr>
-                  <td>{helpline.Service}</td>
-                  <td>{helpline.ContactNumber}</td>
-                  <td>{helpline.Desc}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </Table>
+    
+      <div className="background">
+        <div className="container">
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>Service</Th>
+                <Th>Contact Number</Th>
+                <Th>Description</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {Helplines.map((helpline) => {
+                return (
+                  <Tr>
+                    <Td>{helpline.Service}</Td>
+                    <Td>{helpline.ContactNumber}</Td>
+                    <Td>{helpline.Desc}</Td>
+                  </Tr>
+                )
+              })}
+            </Tbody>
+          </Table>
+        </div>
       </div>
-    </div>
   );
 }
 
