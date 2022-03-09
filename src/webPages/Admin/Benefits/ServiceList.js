b1a82660098d5bbd50967f9f5bfa0dcf0da26f7f
40203858 <emoran05@qub.ac.uk>
2022-03-09 14:46:55 +0000

    All admin update pages now able to update descriptions of services

--- src/webPages/Admin/Benefits/ServiceList.js
+++ src/webPages/Admin/Benefits/ServiceList.js
@@ -27,7 +27,7 @@ const ServiceList = ({ getServiceId }) => {
           Refresh List
         </Button>
       </div>
-      <table>
+      <Table striped bordered responsive="sm">
         <thead>
           <tr>
             <th>Service</th>
@@ -66,7 +66,7 @@ const ServiceList = ({ getServiceId }) => {
             )
           })}
         </tbody>
-      </table>
+      </Table>
     </div>
   );
 };
