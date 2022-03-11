901c23d6ae3c582349da6e30320a3e28aa610069
Caolan Rice <crice42@qub.ac.uk>
2022-03-11 21:11:33 +0000

    adding responsive cards for later use

---
+++ src/components/Cards.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
+
+function Cards() {
+    return (
+        <div className='cards'>
+            <h1>Below are some of the services we offer</h1>
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src='images/logo.jpeg'
+                            text='Click here if you are in Crisis and Require immediate help'
+                            label='Crisis'
+                            path='/crisisHelp'
+                        />
+                        <CardItem
+                            src='images/logo.jpeg'
+                            text='Search for Support'
+                            label='Search'
+                            path='/searchHelp'
+                        />
+                        <CardItem
+                            src='images/logo.jpeg'
+                            text='Support Map'
+                            label='Support Map'
+                            path='/allServicesMap'
+                        />
+                        <CardItem
+                            src='images/logo.jpeg'
+                            text='Contact Us'
+                            label='Contact'
+                            path='/contactUs'
+                        />
+                    </ul>
+
+                    {/* next cards would be placed below the above */}
+                    {/* <ul className='cards__items'>
+            <CardItem
+              src='images/logo.jpeg'
+              text='Support Map'
+              label='Support Map'
+              path='/services'
+            />
+            <CardItem
+              src='images/logo.jpeg'
+              text='Contact Us'
+              label='Contact'
+              path='/contactUs'
+            />
+          </ul> */}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Cards;
