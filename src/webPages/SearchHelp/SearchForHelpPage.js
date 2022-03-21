75596d3caeadda92c8e1508a062b6b20d4afb74b
40203858 <emoran05@qub.ac.uk>
2022-03-21 15:16:53 +0000

    Search page updated

--- src/webPages/SearchHelp/SearchForHelpPage.js
+++ src/webPages/SearchHelp/SearchForHelpPage.js
@@ -7,16 +7,19 @@ function App() {
   return (
 
     <div className="background">
-    <div className="container">
-      <div className="col-md-12">
-        <div className="d-flex justify-content-center">
-            <div className="whiteBackground">
-            <Search />
+      <Container width="80%" fluid background-colour="white">
+        <div className="">
+          <div className="col-md-12">
+            <div className="d-flex justify-content-center">
+              <div className="whiteBackground">
+                <Search />
+              </div>
             </div>
+          </div>
         </div>
-      </div>
-      </div>
-      <Footer/>
+      </Container>
+
+      <Footer />
     </div>
 
 
